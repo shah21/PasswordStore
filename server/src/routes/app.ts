2f7d2ab3b4d402951d122062cf8864e95f379f85
shah21 <muhsinshah21@gmail.com>
2021-04-20 20:05:42 +0530

    added update api

--- server/src/routes/app.ts
+++ server/src/routes/app.ts
@@ -3,7 +3,7 @@
 import { Router } from 'express';
 import { body } from "express-validator";
 
-import { postCreatApp,getApps,getApp,deleteApp } from '../controllers/app';
+import { postCreatApp,getApps,getApp,deleteApp, updateApp } from '../controllers/app';
 import App from '../models/app';
 
 const router = Router();
@@ -13,6 +13,17 @@ router.get('/',getApps);
 router.get('/:appName',getApp);
 
 router.delete('/delete/:appName',deleteApp);
+
+router.patch('/update/:appName',[
+    body('password').not().isEmpty().withMessage('Password is empty'),
+    body('app').not().isEmpty().withMessage('Appname is empty').custom(async (value,{req})=>{
+        /* Check app is already existed or not */
+        const app = await App.findByApp(value);
+        if (app) {
+            return Promise.reject("App already exists!");
+        }
+    }),
+],updateApp);
 
 router.post('/create-app',[
     body('password').not().isEmpty().withMessage('Password is empty'),
